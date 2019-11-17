6b43f16736b35b2a0f5af670b0f67321ce1ed115
tasab <36166292+tasab@users.noreply.github.com>
2019-11-17 17:32:08 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# HM6
-Created with CodeSandbox
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import {
+    NavLink
+  } from "react-router-dom";
+import { rootStore } from '../stores/RootStore'
+import '../index.css'
+
+
+export const NavBar = () => {
+
+const [inputValue, setInputValue] = useState('')
+
+const inputChange = event => {
+    setInputValue(event.target.value)
+}
+
+const addInput = () => {
+    setInputValue('')
+    rootStore.groups.add(inputValue)
+}
+
+    return(
+        <div className='NawBar'>
+            <ul>
+                <li><NavLink to={'/favorite'}>Favorite</NavLink></li>
+                <li><NavLink to={'/all-todos'}>All Todos</NavLink></li>
+                <hr />
+                {rootStore.groups.list.map(item => 
+                <li>
+                    <NavLink to={'/' + item.id}>{item.title}</NavLink>
+                </li>)}
+                <li>
+                    <input type='text' placeholder='Enter Group' onChange={inputChange} value={inputValue}/>
+                    <button onClick={addInput}>Add</button>
+                </li>
+            </ul>
+        </div>
+    )
+}
